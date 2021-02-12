9d79367fd9ca2ebd82c1a5997f92d943505e4365
Ivan Zanolla <ivan.zanolla@gmail.com>
2021-02-12 09:01:22 +0100

    Add project files.

---
+++ WebApiTest/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTest.Models
+{
+    public class User
+    {
+
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string Address { get; set; }
+
+
+ 
+
+    }
+}
